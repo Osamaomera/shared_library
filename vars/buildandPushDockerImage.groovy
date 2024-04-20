a9f29013ef2f22ff442960f8bc52becfc454ffeb
Osama Omera <92927320+Osamaomera@users.noreply.github.com>
2024-04-20 13:20:31 +0200

    Update buildandPushDockerImage.groovy

--- vars/buildandPushDockerImage.groovy
+++ vars/buildandPushDockerImage.groovy
@@ -2,8 +2,8 @@
 def call(String dockerHubCredentialsID, String imageName) {
 
 	// Log in to DockerHub 
-	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'username', passwordVariable: 'password')]) {
-		sh "docker login -u ${username} -p ${password}"
+	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+		sh "docker login -u ${USERNAME} -p ${PASSWORD}"
         }
         
         // Build and push Docker image
