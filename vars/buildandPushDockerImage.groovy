19365164841608dbebfc8eae2c7b175b9bc47da9
Osama Omera <92927320+Osamaomera@users.noreply.github.com>
2024-04-20 13:49:25 +0200

    Update buildandPushDockerImage.groovy

--- vars/buildandPushDockerImage.groovy
+++ vars/buildandPushDockerImage.groovy
@@ -8,6 +8,6 @@ def call(String dockerHubCredentialsID, String imageName, String REPOSITORY) {
         
         // Build and push Docker image
         echo "Building and Pushing Docker image..."
-        sh 'docker build -t ${imageName}:${BUILD_NUMBER} .'
-        sh 'docker push ${imageName}:${BUILD_NUMBER}'	 
+        sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
+        sh "docker push ${imageName}:${BUILD_NUMBER}"	 
 }
